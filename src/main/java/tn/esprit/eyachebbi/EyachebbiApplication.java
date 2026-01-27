294142d160e2947f89b4fad908ce36dd344ffb96
chebbi-eya <zahwa.chebbi@gmail.com>
2026-01-27 11:40:16 +0100

    Initial commit - Spring Boot project

---
+++ src/main/java/tn/esprit/eyachebbi/EyachebbiApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.eyachebbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyachebbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyachebbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/eyachebbi/EyachebbiApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.eyachebbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyachebbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
